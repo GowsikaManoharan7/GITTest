625fbb38b17492422e61c1d943fdd7e42ab4e912
= <=>
2019-07-30 12:05:18 +0530

    First Commit

---
+++ src/Pl1.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class Pl1 {
+	@Test
+	public void mobiletest() {
+		System.out.println("Mobile");
+	}
+	@Test
+	public void WEBtest() {
+		System.out.println("Mobile");
+	}
+}
---
+++ src/pl2.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class pl2 {
+	@Test
+	public void mobiletestp2() {
+		System.out.println("Mobile");
+	}
+	@Test
+	public void WEBtestp2() {
+		System.out.println("Mobile");
+	}
+
+}
